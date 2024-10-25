f9b9f154d0b2d2ac9dd57cdbfd0b63f7d35fee55
Clara B. Oliveira <batistaoliveira.clara@gmail.com>
2024-10-24 22:36:11 -0400

    Adicao da classe, metodo e mensagem de saida

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello, World")
+class hero {
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+       
+    }
+
+    atacar(){
+        let poder =""
+        if (this.tipo == "mago"){
+            poder = "magia"
+        }else if (this.tipo == "guerreiro"){
+            poder = "espada"
+        }else if (this.tipo == "monge"){
+            poder = "artes marciais"
+        }else
+            poder = "shuriken"
+    
+        console.log("O " + this.tipo +  " atacou usando: " + poder)
+    }
+
+
+}
+
+let character = new hero("Joseph", "233", "guerreiro")
+character.atacar()
